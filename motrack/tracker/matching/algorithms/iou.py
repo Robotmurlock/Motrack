56bd0322152e9a1b4c235fd8c2aef2a950beda2a
momiradzemovic <everseen>
2023-12-27 00:04:35 +0100

    Add: Implement Adaptive IoU threshold association algorithm

--- motrack/tracker/matching/algorithms/iou.py
+++ motrack/tracker/matching/algorithms/iou.py
@@ -14,23 +14,18 @@ LabelType = Union[int, str]
 LabelGatingType = Union[LabelType, List[Tuple[LabelType, LabelType]]]
 
 
-@ASSOCIATION_CATALOG.register('iou')
-class IoUAssociation(AssociationAlgorithm):
+class IoUBasedAssociation(AssociationAlgorithm):
     """
-    Solves the linear sum assignment problem from given cost matrix based on IOU scores.
+    Standard negative IoU association with gating.
     """
+
     def __init__(
         self,
-        match_threshold: float = 0.30,
-        fuse_score: bool = False,
         label_gating: Optional[LabelGatingType] = None,
         fast_linear_assignment: bool = False
     ):
         """
         Args:
-            match_threshold: Min threshold do match tracklet with object.
-                If threshold is not met then cost is equal to infinity.
-            fuse_score: Fuse score with iou
             label_gating: Define which object labels can be matched
                 - If not defined, matching is label agnostic
             fast_linear_assignment: Use greedy algorithm for linear assignment
@@ -38,13 +33,10 @@ class IoUAssociation(AssociationAlgorithm):
         """
         super().__init__(fast_linear_assignment=fast_linear_assignment)
 
-        self._match_threshold = match_threshold
-        self._fuse_score = fuse_score
-
         self._label_gating = {tuple(e) for e in label_gating} if label_gating is not None else None
         self._fast_linear_assignment = fast_linear_assignment
 
-    def _can_match(self, tracklet_label: LabelType, det_label: LabelType) -> bool:
+    def _can_match_labels(self, tracklet_label: LabelType, det_label: LabelType) -> bool:
         """
         Checks if matching between tracklet and detection is possible.
 
@@ -67,33 +59,131 @@ class IoUAssociation(AssociationAlgorithm):
             or (det_label, tracklet_label) in self._label_gating
 
     def form_cost_matrix(
-        self,
-        tracklet_estimations: List[PredBBox],
-        detections: List[PredBBox],
-        object_features: Optional[np.ndarray] = None,
-        tracklets: Optional[List[Tracklet]] = None
+            self,
+            tracklet_estimations: List[PredBBox],
+            detections: List[PredBBox],
+            object_features: Optional[np.ndarray] = None,
+            tracklets: Optional[List[Tracklet]] = None
     ) -> np.ndarray:
         _ = object_features  # Unused
 
         n_tracklets, n_detections = len(tracklet_estimations), len(detections)
         cost_matrix = np.zeros(shape=(n_tracklets, n_detections), dtype=np.float32)
         for t_i in range(n_tracklets):
+            tracklet = tracklets[t_i]
             tracklet_bbox = tracklet_estimations[t_i]
 
             for d_i in range(n_detections):
                 det_bbox = detections[d_i]
 
                 # Check if matching is possible
-                if not self._can_match(tracklet_bbox.label, det_bbox.label):
+                if not self._can_match_labels(tracklet_bbox.label, det_bbox.label):
                     cost_matrix[t_i][d_i] = np.inf
                     continue
 
-                # Calculate IOU score
-                iou_score = tracklet_bbox.iou(det_bbox)
-                # Higher the IOU score the better is the match (using negative values because of min optim function)
-                # If score has very high value then
-                score = iou_score * det_bbox.conf if self._fuse_score else iou_score
-                score = - score if score > self._match_threshold else np.inf
-                cost_matrix[t_i][d_i] = score
+                cost_matrix[t_i][d_i] = self._calc_score(tracklet, tracklet_bbox, det_bbox)
 
         return cost_matrix
+
+    def _calc_score(self, tracklet: Tracklet, tracklet_bbox: PredBBox, det_bbox: PredBBox) -> float:
+        """
+        Calculates matching score (cost) between tracklet and a detection bounding box.
+
+        Args:
+            tracklet: Tracklet info
+            tracklet_bbox: Tracklet bounding box
+            det_bbox:
+
+        Returns:
+            Matching score
+        """
+        raise NotImplementedError('Score calculation is not implemented!')
+
+
+@ASSOCIATION_CATALOG.register('iou')
+class IoUAssociation(IoUBasedAssociation):
+    """
+    Standard negative IoU association with gating.
+    """
+    def __init__(
+        self,
+        match_threshold: float = 0.30,
+        fuse_score: bool = False,
+        label_gating: Optional[LabelGatingType] = None,
+        fast_linear_assignment: bool = False
+    ):
+        """
+        Args:
+            match_threshold: Min threshold do match tracklet with object.
+                If threshold is not met then cost is equal to infinity.
+            fuse_score: Fuse score with iou
+            label_gating: Define which object labels can be matched
+                - If not defined, matching is label agnostic
+            fast_linear_assignment: Use greedy algorithm for linear assignment
+                - This might be more efficient in case of large cost matrix
+        """
+        super().__init__(
+            label_gating=label_gating,
+            fast_linear_assignment=fast_linear_assignment
+        )
+
+        self._match_threshold = match_threshold
+        self._fuse_score = fuse_score
+
+    def _calc_score(self, tracklet: Tracklet, tracklet_bbox: PredBBox, det_bbox: PredBBox) -> float:
+        _ = tracklet
+
+        # Calculate IOU score
+        iou_score = tracklet_bbox.iou(det_bbox)
+        # Higher the IOU score the better is the match (using negative values because of min optim function)
+        # If score has very high value then
+        score = iou_score * det_bbox.conf if self._fuse_score else iou_score
+        return - score if score > self._match_threshold else np.inf
+
+
+@ASSOCIATION_CATALOG.register('adaptive-iou')
+class IoUAssociationAdaptiveGating(IoUBasedAssociation):
+    """
+    Adaptive IoU scales the match threshold based on the object's velocity.
+    Object's that have less movement should have more overlap in the next frame.
+    """
+    def __init__(
+        self,
+        match_threshold: float = 0.30,
+        fuse_score: bool = False,
+        gating_factor: float = 1.0,
+        label_gating: Optional[LabelGatingType] = None,
+        fast_linear_assignment: bool = False
+    ):
+        """
+        Args:
+            match_threshold: Min threshold do match tracklet with object.
+                If threshold is not met then cost is equal to infinity.
+            fuse_score: Fuse score with iou
+            gating_factor: Adaptive gating factor
+            label_gating: Define which object labels can be matched
+                - If not defined, matching is label agnostic
+            fast_linear_assignment: Use greedy algorithm for linear assignment
+                - This might be more efficient in case of large cost matrix
+        """
+        super().__init__(
+            label_gating=label_gating,
+            fast_linear_assignment=fast_linear_assignment
+        )
+
+        self._match_threshold = match_threshold
+        self._fuse_score = fuse_score
+        self._gating_factor = gating_factor
+
+    def _calc_score(self, tracklet: Tracklet, tracklet_bbox: PredBBox, det_bbox: PredBBox) -> float:
+        # Estimate variable gating threshold
+        match_threshold = self._match_threshold
+        if len(tracklet.history) >= 2:
+            last_bbox_index, last_bbox = tracklet.history[-1]
+            prev_bbox_index, prev_bbox = tracklet.history[-2]
+            movement = np.abs(last_bbox.as_numpy_xyxy() - prev_bbox.as_numpy_xyxy()).mean()
+            match_threshold -= movement * self._gating_factor
+
+        iou_score = tracklet_bbox.iou(det_bbox)
+        score = iou_score * det_bbox.conf if self._fuse_score else iou_score
+        return - score if score > match_threshold else np.inf
